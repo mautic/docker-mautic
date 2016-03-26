1f7e08c3e51a7678599a1b3f962635a8709008a0
Antonio Milesi Bastos <antonio@milesibastos.com>
2016-03-26 18:20:03 -0300

    fix to bootstrap

--- apache/docker-entrypoint.sh
+++ apache/docker-entrypoint.sh
@@ -69,7 +69,4 @@ if ! [ -e app/config/local.php ]; then
         chown www-data:www-data app/config/local.php
 fi
 
-# Start the cron service in the background. Unfortunately upstart doesnt work yet.
-cron -f &
-
 exec "$@"
