7dd7a24e69ef25f3b7ba12e738b3d8b30b20bcdb
Luiz Eduardo <luizeof@gmail.com>
2018-04-09 08:24:37 -0300

    Download Tester after Mautic Downloaded

--- apache/docker-entrypoint.sh
+++ apache/docker-entrypoint.sh
@@ -14,11 +14,6 @@ if [ -n "$MYSQL_PORT_3306_TCP" ]; then
         fi
 fi
 
-# Github Pull Tester
-if [ -n "$MAUTIC_TESTER" ]; then
-  echo >&2 "Copying Mautic Github Pull Tester"
-  wget https://raw.githubusercontent.com/mautic/mautic-tester/master/tester.php
-fi
 
 if [ -z "$MAUTIC_DB_HOST" ]; then
         echo >&2 "error: missing MAUTIC_DB_HOST and MYSQL_PORT_3306_TCP environment variables"
@@ -127,6 +122,16 @@ fi
 
 echo >&2
 echo >&2 "========================================================================"
+
+
+
+# Github Pull Tester
+if [ -n "$MAUTIC_TESTER" ]; then
+  echo >&2 "Copying Mautic Github Pull Tester"
+  wget https://raw.githubusercontent.com/mautic/mautic-tester/master/tester.php
+fi
+
+
 
 "$@" &
 MAINPID=$!
