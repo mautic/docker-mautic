714900c82d56cf54d846a0324b5d197b77f9cc08
mathieu.brunot <mb.mathieu.brunot@gmail.com>
2019-04-04 02:19:59 +0200

    :sparkles: Use docker template file

--- update.sh
+++ update.sh
@@ -1,24 +1,59 @@
 #!/bin/bash
 set -e
 
-current="$(curl https://api.github.com/repos/mautic/mautic/releases/latest -s | jq -r .name)"
+declare -A cmd=(
+	[apache]='apache2-foreground'
+	[fpm]='php-fpm'
+	[fpm-alpine]='php-fpm'
+)
+
+declare -A extras=(
+	[apache]='\n# Enable Apache Rewrite Module\nRUN a2enmod rewrite'
+	[fpm]=''
+	[fpm-alpine]=''
+)
+
+declare -A base=(
+	[apache]='debian'
+	[fpm]='debian'
+	[fpm-alpine]='alpine'
+)
+
+variants=(
+	apache
+	fpm
+)
+
+echo "get current version"
+current=$( curl -fsSL 'https://api.github.com/repos/mautic/mautic/tags' |tac|tac| \
+	grep -oE '[[:digit:]]+\.[[:digit:]]+\.[[:digit:]]+' | \
+	sort -urV | \
+	head -n 1 )
 
 # TODO - Expose SHA signatures for the packages somewhere
-wget -O mautic.zip https://s3.amazonaws.com/mautic/releases/$current.zip
+echo "get current SHA signature"
+curl -s https://s3.amazonaws.com/mautic/releases/$current.zip -o mautic.zip
 sha1="$(sha1sum mautic.zip | sed -r 's/ .*//')"
 
-for variant in apache fpm; do
-	(
-		set -x
+echo "update docker images"
+for variant in "${variants[@]}"; do
+	dir="$variant"
+	echo "generating $current-$variant"
 
-		sed -ri '
-			s/^(ENV MAUTIC_VERSION) .*/\1 '"$current"'/;
-			s/^(ENV MAUTIC_SHA1) .*/\1 '"$sha1"'/;
-		' "$variant/Dockerfile"
+	template="Dockerfile-${base[$variant]}.template"
+	cp $template "$dir/Dockerfile"
 
-        # To make management easier, we use these files for all variants
-		cp common/* "$variant"/
-	)
+	sed -E -i'' -e '
+		s/%%VARIANT%%/'"$variant"'/;
+		s/%%VARIANT_EXTRAS%%/'"${extras[$variant]}"'/;
+		s/%%VERSION%%/'"$current"'/;
+		s/%%VERSION_SHA1%%/'"$sha1"'/;
+		s/%%CMD%%/'"${cmd[$variant]}"'/;
+	' "$dir/Dockerfile"
+
+	# To make management easier, we use these files for all variants
+	cp common/* "$dir"/
 done
 
+echo "remove mautic.zip"
 rm mautic.zip
