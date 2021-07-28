2d234d4430d3ec2ae5fc0a7aeda8ba9fe70044a6
Björn Marten <bjoern.marten@netresearch.de>
2021-07-28 14:23:13 +0200

    SRVM-13: add testing docker-entrypoint.sh script in order to backup and reapply media files themes and local.php config so data won't get lost after a Mautic update

---
+++ test/docker-entrypoint.sh
@@ -0,0 +1,211 @@
+#!/usr/bin/env bash
+set -ex
+
+BACKUPPATH=/backup
+BASEPATH=/var/www/html
+MEDIA="$BASEPATH/media"
+THEMES="$BASEPATH/themes"
+CONFIG_FILE="$BASEPATH/app/config/local.php"
+
+if [ ! -d "$BACKUPPATH" ]; then
+        mkdir "$BACKUPPATH"
+fi
+
+if [ -d "$BASEPATH" ]; then
+        if [ -d "$MEDIA" ]; then
+                cp -rf "$MEDIA" "$BACKUPPATH/"
+        fi
+        if [ -d "$THEMES" ]; then
+                cp -rf "$THEMES" "$BACKUPPATH/"
+        fi
+        if [ -f "$CONFIG_FILE" ]; then
+                cp -f "$CONFIG_FILE" "$BACKUPPATH/"
+        fi
+fi
+rm -rf $BASEPATH/*
+
+
+if [ ! -f /usr/local/etc/php/php.ini ]; then
+cat <<EOF > /usr/local/etc/php/php.ini
+date.timezone = "${PHP_INI_DATE_TIMEZONE}"
+always_populate_raw_post_data = -1
+memory_limit = ${PHP_MEMORY_LIMIT}
+file_uploads = On
+upload_max_filesize = ${PHP_MAX_UPLOAD}
+post_max_size = ${PHP_MAX_UPLOAD}
+max_execution_time = ${PHP_MAX_EXECUTION_TIME}
+EOF
+fi
+
+
+if [ -n "$MYSQL_PORT_3306_TCP" ]; then
+        if [ -z "$MAUTIC_DB_HOST" ]; then
+                export MAUTIC_DB_HOST='dbms'
+                if [ "$MAUTIC_DB_USER" = 'root' ] && [ -z "$MAUTIC_DB_PASSWORD" ]; then
+                        export MAUTIC_DB_PASSWORD="$MYSQL_ENV_MYSQL_ROOT_PASSWORD"
+                fi
+        else
+                echo >&2 "warning: both MAUTIC_DB_HOST and MYSQL_PORT_3306_TCP found"
+                echo >&2 "  Connecting to MAUTIC_DB_HOST ($MAUTIC_DB_HOST)"
+                echo >&2 "  instead of the linked dbms container"
+        fi
+fi
+
+
+if [ -z "$MAUTIC_DB_HOST" ]; then
+        echo >&2 "error: missing MAUTIC_DB_HOST and MYSQL_PORT_3306_TCP environment variables"
+        echo >&2 "  Did you forget to --link some_dbms_container:dbms or set an external db"
+        echo >&2 "  with -e MAUTIC_DB_HOST=hostname:port?"
+        exit 1
+fi
+
+
+if [ -z "$MAUTIC_DB_PASSWORD" ]; then
+        echo >&2 "error: missing required MAUTIC_DB_PASSWORD environment variable"
+        echo >&2 "  Did you forget to -e MAUTIC_DB_PASSWORD=... ?"
+        echo >&2
+        echo >&2 "  (Also of interest might be MAUTIC_DB_USER and MAUTIC_DB_NAME.)"
+        exit 1
+fi
+
+#ENABLES HUBSPOT CRON
+if [ -n "$MAUTIC_CRON_HUBSPOT" ]; then
+        echo >&2 "CRON: Activating Hubspot"
+        echo "10,40 * * * *     www-data   php /var/www/html/bin/console mautic:integration:fetchleads --integration=Hubspot > /var/log/cron.pipe 2>&1" >> /etc/cron.d/mautic
+        echo "15,45 * * * *     www-data   php /var/www/html/bin/console mautic:integration:pushactivity --integration=Hubspot > /var/log/cron.pipe 2>&1" >> /etc/cron.d/mautic
+fi
+
+#ENABLES SALESFORCE CRON
+if [ -n "$MAUTIC_CRON_SALESFORCE" ]; then
+        echo >&2 "CRON: Activating Salesforce"
+        echo "10,40 * * * *     www-data   php /var/www/html/bin/console mautic:integration:fetchleads --integration=Salesforce > /var/log/cron.pipe 2>&1" >> /etc/cron.d/mautic
+        echo "12,42 * * * *     www-data   php /var/www/html/bin/console mautic:integration:pushactivity --integration=Salesforce > /var/log/cron.pipe 2>&1" >> /etc/cron.d/mautic
+        echo "14,44 * * * *     www-data   php /var/www/html/bin/console mautic:integration:pushleadactivity --integration=Salesforce > /var/log/cron.pipe 2>&1" >> /etc/cron.d/mautic
+        echo "16,46 * * * *     www-data   php /var/www/html/bin/console mautic:integration:synccontacts --integration=Salesforce > /var/log/cron.pipe 2>&1" >> /etc/cron.d/mautic
+fi
+
+#ENABLES SUGARCRM CRON
+if [ -n "$MAUTIC_CRON_SUGARCRM" ]; then
+        echo >&2 "CRON: Activating SugarCRM"
+        echo "10,40 * * * *     www-data   php /var/www/html/bin/console mautic:integration:fetchleads --fetch-all --integration=Sugarcrm > /var/log/cron.pipe 2>&1" >> /etc/cron.d/mautic
+fi
+
+#ENABLES PIPEDRIVE CRON
+if [ -n "$MAUTIC_CRON_PIPEDRIVE" ]; then
+        echo >&2 "CRON: Activating Pipedrive"
+        echo "10,40 * * * *     www-data   php /var/www/html/bin/console mautic:integration:pipedrive:fetch > /var/log/cron.pipe 2>&1" >> /etc/cron.d/mautic
+        echo "15,45 * * * *     www-data   php /var/www/html/bin/console mautic:integration:pipedrive:push > /var/log/cron.pipe 2>&1" >> /etc/cron.d/mautic
+fi
+
+#ENABLES ZOHO CRON
+if [ -n "$MAUTIC_CRON_ZOHO" ]; then
+        echo >&2 "CRON: Activating ZohoCRM"
+        echo "10,40 * * * *     www-data   php /var/www/html/bin/console mautic:integration:fetchleads --integration=Zoho > /var/log/cron.pipe 2>&1" >> /etc/cron.d/mautic
+fi
+
+#ENABLES DYNAMICS CRON
+if [ -n "$MAUTIC_CRON_DYNAMICS" ]; then
+        echo >&2 "CRON: Activating DynamicsCRM"
+        echo "10,40 * * * *     www-data   php /var/www/html/bin/console mautic:integration:fetchleads -i Dynamics > /var/log/cron.pipe 2>&1" >> /etc/cron.d/mautic
+fi
+
+# ToDo
+#
+# 1. Copy source file to working dir
+#    1. ? always? ... to ensure update is processed?
+#    2. ? does this overwerite configuration?
+# 2. Write configuration if not existing
+# 3. Run migration/upgrade
+#    1. ? always? depending on what? an step 2?
+
+if ! [ -e index.php -a -e app/AppKernel.php ]; then
+        # No Mautic source files found in target location
+        # -> New installation
+        echo >&2 "Mautic not found in $(pwd) - copying now..."
+
+        if [ "$(ls -A)" ]; then
+                echo >&2 "WARNING: $(pwd) is not empty - press Ctrl+C now if this is an error!"
+                ( set -x; ls -A; sleep 10 )
+        fi
+
+        tar cf - --one-file-system -C /usr/src/mautic . | tar xf -
+
+        echo >&2 "Complete! Mautic has been successfully copied to $(pwd)"
+fi
+
+echo >&2 "========================================================================"
+echo >&2
+echo >&2 "This server is now configured to run Mautic!"
+
+if ! [ -e app/config/local.php ]; then
+        # Write the database connection to the config so the installer prefills it
+        php /makeconfig.php
+
+        # Make sure our web user owns the config file if it exists
+        chown www-data:www-data app/config/local.php
+        mkdir -p /var/www/html/var/logs
+        chown www-data:www-data /var/www/html/var/logs
+else
+        rm -rf /var/www/html/var/cache/*
+        php /var/www/html/bin/console doctrine:migrations:migrate \
+                --no-interaction \
+                --env=prod \
+                --no-debug
+fi
+
+if [ -f "$BACKUPPATH/local.php" ]; then
+        cp -f "$BACKUPPATH/local.php" "$CONFIG_FILE"
+fi
+
+if [ -d "$BACKUPPATH/media" ]; then
+        cp -ur "$BACKUPPATH/media" "$BASEPATH"
+fi
+
+if [ -d "$BACKUPPATH/themes" ]; then
+        cp -ur "$BACKUPPATH/themes" "$BASEPATH"
+fi
+
+if [ -d "$BACKUPPATH" ]; then
+        rm -rf $BACKUPPATH
+fi
+
+# clear any cache remains
+rm -rf /var/www/html/var/cache/*
+
+# ToDo add health checks
+
+# configure CRON
+if [[ "$MAUTIC_RUN_CRON_JOBS" == "true" ]]; then
+    if [ ! -e /var/log/cron.pipe ]; then
+        mkfifo /var/log/cron.pipe
+        chown www-data:www-data /var/log/cron.pipe
+    fi
+    (tail -f /var/log/cron.pipe | while read line; do echo "[CRON] $line"; done) &
+    CRONLOGPID=$!
+    cron -f &
+    CRONPID=$!
+else
+    echo >&2 "Not running cron as requested."
+fi
+
+echo >&2
+echo >&2 "========================================================================"
+
+"$@" &
+MAINPID=$!
+
+shut_down() {
+    if [[ "$MAUTIC_RUN_CRON_JOBS" == "true" ]]; then
+        kill -TERM $CRONPID || echo 'Cron not killed. Already gone.'
+        kill -TERM $CRONLOGPID || echo 'Cron log not killed. Already gone.'
+    fi
+    kill -TERM $MAINPID || echo 'Main process not killed. Already gone.'
+}
+trap 'shut_down;' TERM INT
+
+# wait until all processes end (wait returns 0 retcode)
+while :; do
+    if wait; then
+        break
+    fi
+done
