dc390af00713c7279efcfc192a60770f05221f73
luizeof <luizeof@gmail.com>
2022-08-20 01:06:48 -0700

    fix assertions option

--- common/docker-entrypoint.sh
+++ common/docker-entrypoint.sh
@@ -5,6 +5,7 @@ if [ ! -f /usr/local/etc/php/php.ini ]; then
 cat <<EOF > /usr/local/etc/php/php.ini
 date.timezone = "${PHP_INI_DATE_TIMEZONE}"
 always_populate_raw_post_data = -1
+zend.assertions = -1
 memory_limit = ${PHP_MEMORY_LIMIT}
 file_uploads = On
 upload_max_filesize = ${PHP_MAX_UPLOAD}
@@ -12,7 +13,6 @@ post_max_size = ${PHP_MAX_UPLOAD}
 max_execution_time = ${PHP_MAX_EXECUTION_TIME}
 EOF
 fi
-
 
 if [ -n "$MYSQL_PORT_3306_TCP" ]; then
         if [ -z "$MAUTIC_DB_HOST" ]; then
