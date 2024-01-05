c24e6b5729c5272d97f483d7278cbf1c4d435bec
Mattias Michaux <mattias.michaux@dropsolid.com>
2024-01-05 13:45:15 +0100

    small changes

--- common/entrypoint_mautic_cron.sh
+++ common/entrypoint_mautic_cron.sh
@@ -1,6 +1,15 @@
 #!/bin/bash
 
-#[[ -f /opt/mautic/cron/mautic ]] || touch /opt/mautic/cron/mautic
+if [ ! -f /opt/mautic/cron/mautic ]; then
+
+	cat <<EOF > /opt/mautic/cron/mautic
+SHELL=/bin/bash
+PATH=/usr/local/sbin:/usr/local/bin:/sbin:/bin:/usr/sbin:/usr/bin
+BASH_ENV=/tmp/cron.env
+
+* * * * * php /var/www/html/bin/console mautic:segments:update 2>&1 | tee /tmp/stdout
+EOF
+fi
 
 # register the crontab file for the www-data user
 crontab -u www-data /opt/mautic/cron/mautic
@@ -12,4 +21,5 @@ chmod 777 /tmp/stdout
 # ensure the PHP env vars are present during cronjobs
 declare -p | grep 'PHP_INI_VALUE_' > /tmp/cron.env
 
+# run cron and print the output
 cron -f | tail -f /tmp/stdout
--- common/entrypoint_mautic_web.sh
+++ common/entrypoint_mautic_web.sh
@@ -1,17 +1,18 @@
 #!/bin/bash
 
+# wait untill the db is fully up before proceeding
 while [[ $(mysqladmin --host=db --user=$MYSQL_USER --password=$MYSQL_PASSWORD ping) != "mysqld is alive" ]]; do
 	sleep 1
 done
 
-# install and demo mautic
-if [ -n "$MAUTIC_TEST_DATA" ] && [ ! -f /var/www/html/config/local.php ]; then
+# generate a local config file if it does't exist
+if [ ! -f /var/www/html/config/local.php ]; then
 
 	su -s /bin/bash www-data -c 'touch /var/www/html/config/local.php'
 
-	cat <<EOF > /var/www/html/config/local.php
+	cat <<'EOF' > /var/www/html/config/local.php
 <?php
-\$parameters = array(
+$parameters = array(
 	'db_driver' => 'pdo_mysql',
 	'db_host' => getenv('MYSQL_HOST'),
 	'db_port' => getenv('MYSQL_PORT'),
@@ -21,16 +22,18 @@ if [ -n "$MAUTIC_TEST_DATA" ] && [ ! -f /var/www/html/config/local.php ]; then
 	'db_table_prefix' => null,
 	'db_backup_tables' => 1,
 	'db_backup_prefix' => 'bak_',
-	'admin_email' => 'mautic@ddev.local',
-    'admin_password' => getenv('MAUTIC_ADMIN_PASSWORD'),
     'messenger_dsn_email' => 'doctrine://default',
     'messenger_dsn_hit' => 'doctrine://default',
 );
 EOF
+fi
 
+# prepare mautic with test data
+if [ "$MAUTIC_LOAD_TEST_DATA" = "true" ]; then
 	su -s /bin/bash www-data -c 'php /var/www/html/bin/console doctrine:migrations:sync-metadata-storage'
-	su -s /bin/bash www-data -c 'php /var/www/html/bin/console mautic:install --force http://localhost'
+	su -s /bin/bash www-data -c 'php /var/www/html/bin/console mautic:install --force --admin_email willchange@mautic.org --admin_password willchange  http://localhost'
 	su -s /bin/bash www-data -c 'php /var/www/html/bin/console doctrine:fixtures:load -n'
 fi
 
+# execute the provided entrypoint
 "$@"
