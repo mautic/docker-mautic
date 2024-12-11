4af5770a82e4cb37714081a62ea8fd15347cad11
Vitor Mattos <vitor@php.rio>
2024-12-11 11:01:30 -0300

    fix: prevent Globbing and Word Splitting
    If the environments have spaces could generate problems at this row
    
    Signed-off-by: Vitor Mattos <vitor@php.rio>

--- .docker/scripts/entrypoint_mautic_web.sh
+++ .docker/scripts/entrypoint_mautic_web.sh
@@ -3,7 +3,7 @@
 chown -R www-data:www-data /var/www/html/config /var/www/html/var/logs /var/www/html/docroot/media
 
 # wait untill the db is fully up before proceeding
-while [[ $(mysqladmin --host=$MAUTIC_DB_HOST --port=$MAUTIC_DB_PORT --user=$MAUTIC_DB_USER --password=$MAUTIC_DB_PASSWORD ping) != "mysqld is alive" ]]; do
+while [[ $(mysqladmin --host="$MAUTIC_DB_HOST" --port="$MAUTIC_DB_PORT" --user="$MAUTIC_DB_USER" --password="$MAUTIC_DB_PASSWORD" ping) != "mysqld is alive" ]]; do
 	sleep 1
 done
 
