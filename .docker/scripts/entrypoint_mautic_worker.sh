e4ed49b477752a57f6e99002591fb37676afa1ed
Vitor Mattos <vitor@php.rio>
2024-12-11 11:01:09 -0300

    chore: ignore error
    Signed-off-by: Vitor Mattos <vitor@php.rio>

--- .docker/scripts/entrypoint_mautic_worker.sh
+++ .docker/scripts/entrypoint_mautic_worker.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 # wait until Mautic is installed
+# shellcheck disable=SC2016
 until php -r 'file_exists("/var/www/html/config/local.php") ? include("/var/www/html/config/local.php") : exit(1); exit(isset($parameters["site_url"]) ? 0 : 1);'; do
 	echo "Mautic not installed, waiting to start workers"
 	sleep 5
