20cb4833b70f2a6d89d017421d954cfc27e47fe7
Matt O'Keefe <matthew.okeefe@allergan.com>
2025-04-30 19:28:40 -0500

    Update common/entrypoint_mautic_cron.sh

--- common/entrypoint_mautic_cron.sh
+++ common/entrypoint_mautic_cron.sh
@@ -10,14 +10,13 @@ PATH=/usr/local/sbin:/usr/local/bin:/sbin:/bin:/usr/sbin:/usr/bin
 BASH_ENV=/tmp/cron.env
 
 # Segments Update - every 15 min
-*/15 * * * * php /var/www/html/bin/console mautic:segments:update 2>&1 | tee /tmp/stdout
-
+0,15,30,45 * * * * php /var/www/html/bin/console mautic:segments:update 2>&1 | tee /tmp/stdout
 
 # Campaigns Update - every 15 min starting at 5 past the hour
-5-59/15 * * * * php /var/www/html/bin/console mautic:campaigns:update 2>&1 | tee /tmp/stdout
+5,20,35,50 * * * * php /var/www/html/bin/console mautic:campaigns:update 2>&1 | tee /tmp/stdout
 
 # Trigger Update - every 15 min starting at 10 past the hour
-10-59/15 * * * * php /var/www/html/bin/console mautic:campaigns:trigger 2>&1 | tee /tmp/stdout
+10,25,40,55 * * * * php /var/www/html/bin/console mautic:campaigns:trigger 2>&1 | tee /tmp/stdout
 
 # E-mail Queue - every 1 min - fix 310
 * * * * * php /var/www/html/bin/console messenger:consume email 2>&1 | tee /tmp/stdout
