da289b613bc0f0356e5c6e77f330489a1f9ef958
Matt O'Keefe <Matt@OKeefe.dev>
2025-05-08 10:11:27 -0500

    dead line cleanup

--- common/pre-run-checks.sh
+++ common/pre-run-checks.sh
@@ -94,6 +94,5 @@ done
 # generate a local config file if it doesn't exist.
 # This is needed to ensure the db credentials can be prefilled in the UI, as env vars aren't taken into account.
 if [ ! -f $MAUTIC_VOLUME_CONFIG/local.php ]; then
-	cp -p /local.php > $MAUTIC_VOLUME_CONFIG/
-EOF
+	cp -p /local.php > $MAUTIC_VOLUME_CONFIG/local.php
 fi
