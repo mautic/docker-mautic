48417213a12690e5c63ab1969b13c047d8251782
Matt O'Keefe <Matt@OKeefe.dev>
2025-05-20 11:32:28 -0500

    fixing some pathing issues

--- common/startup/check_local_php_exists.sh
+++ common/startup/check_local_php_exists.sh
@@ -3,5 +3,5 @@
 # generate a local config file if it doesn't exist.
 # This is needed to ensure the db credentials can be prefilled in the UI, as env vars aren't taken into account.
 if [ ! -f $MAUTIC_VOLUME_CONFIG/local.php ]; then
-	cp -p /startup/templates/local.php $MAUTIC_VOLUME_CONFIG/local.php
+	cp -p /templates/local.php $MAUTIC_VOLUME_CONFIG/local.php
 fi
