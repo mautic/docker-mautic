3e94e90ef3912b0c0aff2a806c04f0b4e1b3a3a7
Matt O'Keefe <Matt@OKeefe.dev>
2025-06-23 14:44:54 -0500

    Fixing issue with checks

--- common/startup/check_database_connection.sh
+++ common/startup/check_database_connection.sh
@@ -1,38 +1,37 @@
 #!/bin/bash
 
 function check_database_connection {
-	local IS_MYSQL_ALIVE=false
-	local FAILURE_COUNT=0
-	local MAX_FAILURE_COUNT=31
+  local IS_MYSQL_ALIVE=false
+  local FAILURE_COUNT=0
+  local MAX_FAILURE_COUNT=31
 
-	while [[ "${IS_MYSQL_ALIVE}" != "mysqld is alive" ]]; do
-		FAILURE_COUNT=$((FAILURE_COUNT + 1))
-		if [[ $FAILURE_COUNT -gt $MAX_FAILURE_COUNT ]]; then
-		echo "----- Startup Checks Error Found -----" >&2
-			echo "MySQL is not responding after ${MAX_FAILURE_COUNT} attempts. Exiting." >&2
-			echo "Please ensure the MySQL server is running and accessible from this container." >&2
-			exit 1
-		fi
+  while [[ "${IS_MYSQL_ALIVE}" != "mysqld is alive" ]]; do
+    FAILURE_COUNT=$((FAILURE_COUNT + 1))
+    if [[ $FAILURE_COUNT -gt $MAX_FAILURE_COUNT ]]; then
+    echo "----- Startup Checks Error Found -----" >&2
+      echo "MySQL is not responding after ${MAX_FAILURE_COUNT} attempts. Exiting." >&2
+      echo "Please ensure the MySQL server is running and accessible from this container." >&2
+      exit 1
+    fi
 
-		if [[ "${IS_MYSQL_ALIVE}" =~ "error" ]]; then
-			echo "Error: MySQL response contained error: ${IS_MYSQL_ALIVE}" >&2
-			echo "We will continue to retry the connection."
-		else
-			echo "MySQL is not ready yet, waiting..."
-		fi
-		sleep 1
+    if [[ "${IS_MYSQL_ALIVE}" =~ "error" ]]; then
+      echo "Error: MySQL response contained error: ${IS_MYSQL_ALIVE}" >&2
+      echo "We will continue to retry the connection."
+    else
+      echo "MySQL is not ready yet, waiting..."
+    fi
+    sleep 1
 
-		# try the connection
-		check_mysql_connection
-	done
+    # try the connection
+    check_mysql_connection
+  done
 
 }
 
 function check_mysql_connection {
-	# Make use of lexical scoping of the local IS_MYSQL_ALIVE variable
-	local MAUTIC_DB_PORT="${MAUTIC_DB_PORT:-3306}"
-	[[ $DEBUG -eq 1 ]] && echo "Checking DB connection to ${MAUTIC_DB_HOST}:${MAUTIC_DB_PORT} with user ${MAUTIC_DB_USER}"
-	IS_MYSQL_ALIVE=$(mysqladmin --host="${MAUTIC_DB_HOST}" --port="${MAUTIC_DB_PORT}" --user="${MAUTIC_DB_USER}" --password="${MAUTIC_DB_PASSWORD}" ping 2>&1)
+  # Make use of lexical scoping of the local IS_MYSQL_ALIVE variable
+  [[ $DEBUG -eq 1 ]] && echo "Checking DB connection to ${MAUTIC_DB_HOST}:${MAUTIC_DB_PORT} with user ${MAUTIC_DB_USER}"
+  IS_MYSQL_ALIVE=$(mysqladmin --host="${MAUTIC_DB_HOST}" --port="${MAUTIC_DB_PORT}" --user="${MAUTIC_DB_USER}" --password="${MAUTIC_DB_PASSWORD}" ping 2>&1)
 }
 
 check_database_connection
--- common/startup/check_volumes_exist_ownership.sh
+++ common/startup/check_volumes_exist_ownership.sh
@@ -1,42 +1,43 @@
 #!/bin/bash
 
 function check_volumes_exist_ownership {
-	local VOLUME_ERROR_FOUND=0
-	for volume in ${MAUTIC_VOLUMES}; do
-		if [[ ! -d "${volume}" ]]; then
-			echo "Error: ${volume} does not exist or is not a directory."
-			VOLUME_ERROR_FOUND=1
-		fi
-	done
+    local VOLUME_ERROR_FOUND=0
+  for volume in ${MAUTIC_VOLUMES}; do
+    if [[ ! -d "${volume}" ]]; then
+      echo "Error: ${volume} does not exist or is not a directory."
+      VOLUME_ERROR_FOUND=1
+    fi
+  done
 
-	if [[ $VOLUME_ERROR_FOUND -eq 1 ]]; then
-		echo "----- Startup Checks Error Found -----" >&2
-		echo "Please ensure the volume(s) are mounted correctly and is accessible." >&2
-		echo "If you are running this container as a non-root user, you may need to run the container with elevated privileges." >&2
-		exit 1
-	fi
+  if [[ $VOLUME_ERROR_FOUND -eq 1 ]]; then
+    echo "----- Startup Checks Error Found -----" >&2
+    echo "Please ensure the volume(s) are mounted correctly and is accessible." >&2
+    echo "If you are running this container as a non-root user, you may need to run the container with elevated privileges." >&2
+    exit 1
+  fi
 
-	PERM_ERROR_FOUND=0
-	local VOLUME_PERMISSIONS_FAILURE_MESSAGE
-	for volume in ${MAUTIC_VOLUMES}; do
-	# check ownership and assign ownership if not MAUTIC_WWW_USER:MAUTIC_WWW_GROUP
-		if [[ $(stat -c '%U:%G' "${volume}") != "${MAUTIC_WWW_USER}:${MAUTIC_WWW_GROUP}" ]]; then
-			chown -R "${MAUTIC_WWW_USER}:${MAUTIC_WWW_GROUP}" "${volume}" || {
-				echo "Error: Permissions for ${MAUTIC_WWW_USER}:${MAUTIC_WWW_GROUP} could not be set on ${volume}." >&2
-				PERM_ERROR_FOUND=1
-			}
-		fi
-	done
+  PERM_ERROR_FOUND=0
+  local VOLUME_PERMISSIONS_FAILURE_MESSAGE
+  for perm_volume in ${MAUTIC_VOLUMES}; do
+  # check ownership and assign ownership if not MAUTIC_WWW_USER:MAUTIC_WWW_GROUP
+    if [[ $(stat -c '%U:%G' "${perm_volume}") != "${MAUTIC_WWW_USER}:${MAUTIC_WWW_GROUP}" ]]; then
+      chown -R "${MAUTIC_WWW_USER}:${MAUTIC_WWW_GROUP}" "${perm_volume}" || {
+        echo "Error: Permissions for ${MAUTIC_WWW_USER}:${MAUTIC_WWW_GROUP} could not be set on ${perm_volume}." >&2
+        PERM_ERROR_FOUND=1
+      }
+    fi
+  done
 
-		if [[ $PERM_ERROR_FOUND -eq 1 ]]; then
-		echo "----- Startup Checks Error Found -----" >&2
-		echo "Please ensure the volume(s) is mounted correctly and the ownership/perms are correctly configured (chown ${MAUTIC_WWW_USER}:${MAUTIC_WWW_GROUP})." >&2
-		echo "If you are running this container as a non-root user, you may need to run the container with elevated privileges." >&2
-		exit 1
-	else
-		[[ $DEBUG -eq 1 ]] && echo "All required volumes exist and have correct ownership."
-	fi
-
+  if [[ $PERM_ERROR_FOUND -eq 1 ]]; then
+    echo "----- Startup Checks Error Found -----" >&2
+    echo "Please ensure the volume(s) is mounted correctly and the ownership/perms are correctly configured (chown ${MAUTIC_WWW_USER}:${MAUTIC_WWW_GROUP})." >&2
+    echo "If you are running this container as a non-root user, you may need to run the container with elevated privileges." >&2
+    exit 1
+  else
+    [[ $DEBUG -eq 1 ]] && echo "All required volumes exist and have correct ownership."
+  fi
 }
 
+
+[[ $DEBUG -eq 1 ]] && echo "Checking if required volumes exist and have correct ownership..."
 check_volumes_exist_ownership
