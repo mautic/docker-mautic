7c65c0903f6674a9a03632cc261253fedff0d3aa
Renato <102629460+cibero42@users.noreply.github.com>
2025-08-15 11:36:51 +0200

    Update common/entrypoint_mautic_web.sh
    Co-authored-by: Martin Vooremäe <martin.vooremae@gmail.com>

--- common/entrypoint_mautic_web.sh
+++ common/entrypoint_mautic_web.sh
@@ -13,7 +13,7 @@ fi
 # run migrations
 if php -r "include('${MAUTIC_VOLUME_CONFIG}/local.php'); exit(isset(\$parameters['site_url']) ? 0 : 1);"; then
   log "[${DOCKER_MAUTIC_ROLE}]: Mautic is already installed, running migrations..."
-  su -s /bin/bash $MAUTIC_WWW_USER -c "php $MAUTIC_CONSOLE doctrine:migration:migrate -n"
+  su -s /bin/bash $MAUTIC_WWW_USER -c "php $MAUTIC_CONSOLE doctrine:migrations:migrate -n"
 else
   log "[${DOCKER_MAUTIC_ROLE}]: Mautic is not installed, skipping migrations."
 fi
